43cc2337f1115252e5d96b472487564d58af137f
Javier Alejandro Peralta <javier.peralta666@gmail.com>
2020-02-20 15:31:46 -0300

    User credentials update.

--- samples/success_payment.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-var_dump($_REQUEST['transaction_id']);
--- samples/tickets.php
+++ samples/tickets.php
@@ -1,10 +1,11 @@
 <?php
+error_reporting(E_ALL);
 use InesPostventa\InesPostventa;
 
 require '../InesPostventa.php';
 
 //Send credentials to the sdk server for authentication.
-$powerPayments = new InesPostventa('postventa@suenolar.com.ar', '1c3b7c04cb1ff3361a632b3131960543b8b37c36fd770ac74e8395702285ff5adf8aa4ded7373a7aed2f8d306757237bb2b96dbae396d2f4b5d64199');
+$powerPayments = new InesPostventa('jperalta.powerbyte@gmail.com', '1c3b7c04cb1ff3361a632b3131960543b8b37c36fd770ac74e8395702285ff5adf8aa4ded7373a7aed2f8d306757237bb2b96dbae396d2f4b5d64199');
 
 $tickets = $powerPayments->tickets();
 
